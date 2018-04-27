1bf9afe6571ec3e388ad0de15f25d35ca41e051e
jzxin <19877456@qq.com>
2018-04-27 15:36:16 +0800

    work

---
+++ src/work/work_02.java
@@ -0,0 +1,14 @@
+package work;
+import java.util.Scanner;
+public class work_02{
+	public static void main(String[] srgs) {
+
+		Scanner input=new Scanner(System.in);
+		System.out.println("ÊäÈëĞĞÊı£¡");
+		input.close();
+		int a=1 , d ,i=(int)input.nextInt();
+		for(a=1;a<=i;a++) {
+					for(a=1;a<i;a++){System.out.print(" ");
+					for(d=i;d>1;d--){System.out.print("*");}}
+					System.out.println("");
+	}}}
---
+++ src/work/work_03.java
@@ -0,0 +1,17 @@
+package work;
+
+import java.util.Scanner;
+
+public class work_03 {
+	public static void main(String[] srgs) {
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("请输入一个正整数：");
+		int a = 1, s = 1, i = input.nextInt();
+		input.close();
+		for (; a <= i; a++) {
+			s = s * a;
+		}
+		System.out.print(i + "的阶乘为：" + s);
+	}
+}
