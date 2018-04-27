4fb26703495daa1e4f635da18810875a64af3c07
jzxin <19877456@qq.com>
2018-04-27 17:00:47 +0800

    work

---
+++ src/work/work_06.java
@@ -0,0 +1,13 @@
+package work;
+
+public class work_06 {
+	public static void main(String[] args) {
+	int i=1;
+	do {
+		if(i%2==0) {System.out.print(i+",  ");i++;
+		}else {i++;}
+		
+	}while(i<=100);
+	i=i-1;
+}}
+
