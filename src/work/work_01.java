0d3bd0f2c60da86391596ade94f112387c29eebb
jzxin <19877456@qq.com>
2018-04-27 14:11:19 +0800

    work

---
+++ src/work/work_01.java
@@ -0,0 +1,14 @@
+package work;
+
+public class ¾Å¾Å³Ë·¨±í {
+
+	public static void main(String[] srgs) {
+	int a=1, b=1 ;
+	for(a=1;a<=9;a++) {
+		for(b=1;b<=a;b++) {
+ 			System.out.print(b+ "*" + a+"="+a*b+"   ");
+		}
+		System.out.println("  ");
+	}
+
+}}
