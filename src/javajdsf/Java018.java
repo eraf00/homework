998f237bac1cc629605870c66ca3bc87cbfa7b5b
Administrator <Administrator@PC-20180409IVPG>
2018-05-05 20:49:22 +0800

    Work

--- src/javajdsf/Java018.java
+++ src/javajdsf/Java018.java
@@ -2,8 +2,11 @@ package javajdsf;
 
 public class Java018 {
 	public static void main(String[] srgs) {
-		int a,b,c;
+		int a=1,b=2,c=3;
 		int x,y,z;
-		
+		int i=0;
+		for(;i<=3;i++) {
+			
+		}
 }
 }
--- src/javajdsf/Java019.java
+++ src/javajdsf/Java019.java
@@ -1,5 +1,13 @@
 package javajdsf;
-
 public class Java019 {
-
+	public static void main(String[] srgs) {
+		
+		int i=1,j=7;
+		for(;i<j;i++) {
+		for(int z=1;z<=i;z++) {
+			System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
 }
