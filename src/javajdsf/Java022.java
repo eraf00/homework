55756d1c38a931b4e672cbff3a28080ec317dec1
Administrator <Administrator@PC-20180409IVPG>
2018-05-05 21:02:17 +0800

    Work

--- src/javajdsf/Java022.java
+++ src/javajdsf/Java022.java
@@ -1,5 +1,18 @@
 package javajdsf;
 
 public class Java022 {
+	public static void main(String[] srg) {
+		System.out.print(fac(5));
+		
+		
+		
+		
+		
+	}
 
+	private static int fac(int fn) {
+		if (fn==1)
+		return 1;
+		else fn=fn*fac(fn-1);
+	}
 }
