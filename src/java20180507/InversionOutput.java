dafe053ba5075a9854ab821ff943fed2b08a5ea0
Administrator <Administrator@PC-20180409IVPG>
2018-05-07 16:48:33 +0800

    Work

--- src/java20180507/InversionOutput.java
+++ src/java20180507/InversionOutput.java
@@ -9,9 +9,27 @@ public class InversionOutput {
 		Scanner input = new Scanner(System.in);
 		System.out.println("输入一个正整数：");
 		int score = input.nextInt();
+		String score1 = input.nextLine();
+		int s = score1.length();
+		int right = 1;
 		input.close();
-		char c=(char)score;
-		int right=c.length;//权
-		for()
+		for (; s > 1; s--) {
+			right *= 10;//权为位数减一
+		}
+		int rightmax=right;
+		int i[] = new int[s + 1];
+		i[0] = 0;
+		for (int z = 1 ; z <= s; z++) {// c.length;//权
+			i[z] = score / right - i[z - 1]*10;//从最高位提取数值
+			right/=10;//权递减
+		}
+		int sum=0;
+		right=rightmax;
+		for (int z = s; z > 1; z--) {
+			sum+=i[z]*right;
+			right/=10;//权递减
+		}
+
+		System.out.println(sum);
 	}
 }
