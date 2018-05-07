90fa8abcdfc7c6400b0f7336056fb2b8e6b5db8a
Administrator <Administrator@PC-20180409IVPG>
2018-05-07 21:51:46 +0800

    Work

--- src/javajdsf/Java019.java
+++ src/javajdsf/Java019.java
@@ -1,13 +1,15 @@
 package javajdsf;
+
 public class Java019 {
 	public static void main(String[] srgs) {
-		
-		int i=1,j=7;
-		for(;i<j;i++) {
-		for(int z=1;z<=i;z++) {
-			System.out.print("*");
+		int[] xing = { 1, 3, 6, 8, 6, 3, 1 };
+		for (int i = 0; i < xing.length; i++) {
+			for (int j = 1; j <= xing[i]; j++) {
+				System.out.print("*");
 			}
 			System.out.println("");
 		}
+
 	}
+
 }
