4e1240fab36f43a48e17f5e08dcd2da9181e1b47
Administrator <Administrator@PC-20180409IVPG>
2018-05-07 21:12:38 +0800

    Work

--- src/javajdsf/Java018.java
+++ src/javajdsf/Java018.java
@@ -2,10 +2,21 @@ package javajdsf;
 
 public class Java018 {
 	public static void main(String[] srgs) {
-		char[] A=new char[3];
-		char[] B=new char[3];
-		for(int i=0;i<=2;i++) {
+		
+		String[] yi= {"x","y","z"};
+		String a,b,c;
+	
+		for(int j=0;j<=2;j++) {
+			a=yi[j];
+			for(int q=0;q<=2;q++) {
+				if(q==j)continue;
+				b=yi[q];
+				for(int k=0;k<=2;k++) {
+					if(k==j||k==q)continue;
+					c=yi[k];
+				if(a==yi[0]||c==yi[0]||c==yi[2])continue;
+				      System.out.println("a和"+yi[j]+"比;b和"+yi[q]+"比;c和"+yi[k]+"比。");
 			
-		}
+		}}
 }
-}
+}}
