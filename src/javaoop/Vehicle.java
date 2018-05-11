3f26eca38c9459ac9e44d1f3263e90d7f810fed1
Administrator <Administrator@PC-20180409IVPG>
2018-05-11 16:19:51 +0800

    Work

--- src/javaoop/Vehicle.java
+++ src/javaoop/Vehicle.java
@@ -1,5 +1,21 @@
 package javaoop;
+
 //交通工具(Vehicle)
 public class Vehicle {
 
+	public static void main(String[] args) {
+		VehicleClass vehicle = new VehicleClass();
+		vehicle.setName("汽车");
+		vehicle.setSpeed(200);
+		vehicle.setSize("5*3");
+		vehicle.setColor("blue");
+		vehicle.move();
+		vehicle.speed(400);
+		vehicle.speedup();
+		vehicle.speeddown();
+		System.out.println(vehicle.getName() + " " + vehicle.getColor() + " " + vehicle.getSpeed() + " "
+				+ vehicle.getSize() + " ");
+		VehicleClass c = new VehicleClass("Audi", 800, "800*23", "blue");
+		c.show();
+	}
 }
