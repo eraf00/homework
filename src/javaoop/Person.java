2f7be9f2b8d716688997d2bf8b9bc77e9952363f
Administrator <Administrator@PC-20180409IVPG>
2018-05-13 12:40:01 +0800

    Work

--- src/javaoop/Person.java
+++ src/javaoop/Person.java
@@ -1,5 +1,17 @@
 package javaoop;
+
 //定义一个人类（Person）
 public class Person {
+
+	public static void main(String[] args) {
+		PersonClass man1 = new PersonClass();
+		man1.setName("白泽");
+		man1.setAge(18);
+		man1.display();
+		PersonClass man2 = new PersonClass();
+		man2.setName("白曦");
+		man2.setAge(500);
+		man2.display();
+	}
 
 }
