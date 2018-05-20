fb6bf4fdcab46d460b296281c62588bf463a5468
Administrator <Administrator@PC-20180409IVPG>
2018-05-20 12:56:29 +0800

    Work

--- Main homework/javaoop1/Plural.java
+++ Main homework/javaoop1/Plural.java
@@ -14,27 +14,53 @@ public class Plural {
 		this.thefalse = thefalse;
 
 	}
+	// -------------------------------
 
-	void add(Plural a, Plural b) {
-		System.out.println((a.real + b.real) + "+" + (a.thefalse + b.thefalse) + "i");
+	Plural add(Plural a, Plural b) {
+		Plural c = new Plural((a.real + b.real), (a.thefalse + b.thefalse));
+		return c;
+		// System.out.println("a + b=" + (a.real + b.real) + "+" + (a.thefalse +
+		// b.thefalse) + "i");
+
 	}
 
-	void reduce(Plural a, Plural b) {
-		System.out.println((a.real - b.real) + "+" + (a.thefalse - b.thefalse) + "i");
+	Plural reduce(Plural a, Plural b) {
+		Plural c = new Plural((a.real - b.real), (a.thefalse - b.thefalse));
+		return c;
+
+		// System.out.println("a - b=" + (a.real - b.real) + "+" + (a.thefalse -
+		// b.thefalse) + "i");
 	}
 
-	void ride(Plural a, Plural b) {
-		System.out.println(
-				(a.real * b.real - a.thefalse * b.thefalse) + "+" + (a.real * b.thefalse + a.thefalse * b.real) + "i");
+	Plural ride(Plural a, Plural b) {
+		if (b.real != 0 || b.thefalse != 0) {
+			Plural c = new Plural((a.real * b.real - a.thefalse * b.thefalse),
+					(a.real * b.thefalse + a.thefalse * b.real));
+			return c;
+			// System.out.println("a * b=" + (a.real * b.real - a.thefalse * b.thefalse) +
+			// "+"
+			// + (a.real * b.thefalse + a.thefalse * b.real) + "i");
+		} else {
+			Plural c = new Plural(0, 0);
+			// System.out.println("a * b=0");
+			return c;
+		}
 	}
 
-	void except(Plural a, Plural b) {
-		if (b.real != 0 && b.thefalse != 0)
-			System.out.println((a.real * b.real + a.thefalse * b.thefalse) / (b.real * b.real + b.thefalse * b.thefalse)
-					+ "+" + (a.real * b.thefalse - a.thefalse * b.real) / (b.real * b.real + b.thefalse * b.thefalse)
-					+ "i");
-		else
-			System.out.println("´íÎó£¡");
+	Plural except(Plural a, Plural b) {
+		if (b.real != 0 || b.thefalse != 0) {
+			Plural c = new Plural(
+					(a.real * b.real + a.thefalse * b.thefalse) / (b.real * b.real + b.thefalse * b.thefalse),
+					(a.real * b.thefalse - a.thefalse * b.real) / (b.real * b.real + b.thefalse * b.thefalse));
+			return c;
+			/*
+			 * System.out.println("a / b=" + (a.real * b.real + a.thefalse * b.thefalse) /
+			 * (b.real * b.real + b.thefalse * b.thefalse) + "+" + (a.real * b.thefalse -
+			 * a.thefalse * b.real) / (b.real * b.real + b.thefalse * b.thefalse) + "i");
+			 */
+		} else {
+			return new Plural();
+		}
 	}
 
 	// --------------------
