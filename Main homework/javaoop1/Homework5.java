919d8e5dc09f173a1d21ebe5807f019fef14aa7b
Administrator <Administrator@PC-20180409IVPG>
2018-05-20 13:57:36 +0800

    Work

--- Main homework/javaoop1/Homework5.java
+++ Main homework/javaoop1/Homework5.java
@@ -3,6 +3,13 @@ package javaoop1;
 public class Homework5 {
 
 	public static void main(String[] srgs) {
+		BoxWeight box1 = new BoxWeight();
+		box1 = new BoxWeight(30, 10, 10, 34);
+		System.out.println("盒子box1的体积:" + (box1.getWidth() * box1.getHeight() * box1.getDepth()));
+		System.out.println("盒子box1的重量:" + box1.getWeight());
 
+		BoxWeight box = new BoxWeight(30, 10, 10, 34);
+		System.out.println("盒子box1的体积:" + (box.getWidth() * box.getHeight() * box.getDepth()));
+		box = new BoxWeight();
 	}
 }
