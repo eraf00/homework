1fa9f8e94a4d53396b9193ae903c58ece80c0cbf
Administrator <Administrator@PC-20180409IVPG>
2018-05-20 14:38:07 +0800

    Work

--- Main homework/javaoop1/Homework4.java
+++ Main homework/javaoop1/Homework4.java
@@ -2,4 +2,10 @@ package javaoop1;
 
 public class Homework4 {
 
+	public static void main(String[] srgs) {
+		Pet kate = new Pet("¿¨ÌØ", 6, true, false);
+
+		Pet katelingna = new Pet("¿¨ÌØÁáÄÈ", 6, false, false);
+		kate.Marry(kate, katelingna);
+	}
 }
